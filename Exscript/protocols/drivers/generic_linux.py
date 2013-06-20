c7fb11e078236f3a603429ba1a441923a77df82a
Raffaele <raffaele.forte@gmail.com>
2013-06-20 17:12:51 +0200

    Added generi_linux driver

---
+++ Exscript/protocols/drivers/generic_linux.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2007-2010 Raffaele Forte.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+A generic shell driver that handles Linux shells.
+"""
+import re
+from Exscript.protocols.drivers.driver import Driver
+
+_prompt_re   = [re.compile(r'[\r\n]*([^#]+[#]) ?$')]
+
+class GenericLinuxDriver(Driver):
+    def __init__(self):
+        Driver.__init__(self, 'generic_linux')
+        self.prompt_re   = _prompt_re
+
+    def check_head_for_os(self, string):
+        if _prompt_re[0].search(string):
+            return 50
+        return 0
+
